5afb376c2371349b1b94a81c6d07b4acaa94857c
Kellylin1050 <kellylin1050@gmail.com>
2022-07-20 10:55:48 +0800

    商品功能 - 實作查詢商品列表（查詢條件）

--- src/main/java/com/kelly/springbootmall/controller/ProductController.java
+++ src/main/java/com/kelly/springbootmall/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.kelly.springbootmall.controller;
 
+import com.kelly.springbootmall.constant.ProductCategory;
 import com.kelly.springbootmall.dto.ProductRequest;
 import com.kelly.springbootmall.model.Product;
 import com.kelly.springbootmall.service.ProductService;
@@ -18,8 +19,11 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products") //查詢商品列表
-    public ResponseEntity<List<Product>> getProducts(){
-        List<Product> productList = productService.getProducts();
+    public ResponseEntity<List<Product>> getProducts(
+           @RequestParam(required = false) ProductCategory category,
+           @RequestParam(required = false) String search
+    ){
+        List<Product> productList = productService.getProducts(category,search);
 
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
--- src/main/java/com/kelly/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/kelly/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kelly.springbootmall.service.impl;
 
+import com.kelly.springbootmall.constant.ProductCategory;
 import com.kelly.springbootmall.dao.ProductDao;
 import com.kelly.springbootmall.dto.ProductRequest;
 import com.kelly.springbootmall.model.Product;
@@ -16,8 +17,8 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProducts() {
-        return productDao.getProducts();
+    public List<Product> getProducts(ProductCategory category, String search) {
+        return productDao.getProducts(category, search);
     }
 
     @Override
