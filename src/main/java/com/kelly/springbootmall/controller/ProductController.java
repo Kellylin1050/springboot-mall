698304f81267405109566e34ca5b0a02b0dbcd47
Kellylin1050 <kellylin1050@gmail.com>
2022-07-20 09:36:19 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/kelly/springbootmall/controller/ProductController.java
+++ src/main/java/com/kelly/springbootmall/controller/ProductController.java
@@ -16,7 +16,7 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/products/{productId}")
+    @GetMapping("/products/{productId}") ////查詢商品
     public ResponseEntity<Product> getProduct(@PathVariable Integer productId){
         Product product = productService.getProductById(productId);
         if (product != null) {
@@ -26,7 +26,7 @@ public class ProductController {
         }
     }
 
-    @PostMapping("/products")
+    @PostMapping("/products")   //新增商品
     public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductRequest productRequest){
         Integer productId = productService.createProduct(productRequest);
 
@@ -35,4 +35,23 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
 
     }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                @RequestBody @Valid ProductRequest productRequest){
+        //檢查 product 是否存在
+        Product product = productService.getProductById(productId);
+
+        if (product == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        //修改商品的數據
+        productService.updateProduct(productId,productRequest);
+
+        Product updatedProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+
+    }
+
 }
