0329088eb1d4dd6b3b105bd8caa5ea821b309b87
Kellylin1050 <kellylin1050@gmail.com>
2022-07-29 15:05:01 +0800

    帳號功能 - 登入

--- src/main/java/com/kelly/springbootmall/controller/UserController.java
+++ src/main/java/com/kelly/springbootmall/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.kelly.springbootmall.controller;
 
+import com.kelly.springbootmall.dto.UserLoginRequest;
 import com.kelly.springbootmall.dto.UserRegisterRequest;
 import com.kelly.springbootmall.model.User;
 import com.kelly.springbootmall.service.UserService;
@@ -27,5 +28,12 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
 
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest){
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
--- src/main/java/com/kelly/springbootmall/service/UserService.java
+++ src/main/java/com/kelly/springbootmall/service/UserService.java
@@ -1,5 +1,6 @@
 package com.kelly.springbootmall.service;
 
+import com.kelly.springbootmall.dto.UserLoginRequest;
 import com.kelly.springbootmall.dto.UserRegisterRequest;
 import com.kelly.springbootmall.model.User;
 
@@ -8,4 +9,6 @@ public interface UserService {
     User getUserById(Integer userId);
 
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 }
